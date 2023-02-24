7ddbfad4087577df9684c92a2dd2257c977cf764
Dale-Black <djblack@uci.edu>
2023-02-24 13:38:46 -0800

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using DT
 using Test
 
-# @testset "Scipy 1D" begin
-#     x = [1, 1, 0, 0]
-#     test = transform(x, Scipy())
-#     answer = transform(x, Maurer())
-#     @test test == answer
-# end
+@testset "Scipy 1D" begin
+    x = [1, 1, 0, 0]
+    test = transform(x, Scipy())
+    answer = transform(x, Maurer())
+    @test test == answer
+end
